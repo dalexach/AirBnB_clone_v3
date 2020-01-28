d59ceed95403ac32a507c2fcca5e6f3410857932
jdarangop <juandiegoap33@gmail.com>
2020-01-28 04:47:16 +0000

    Feature: Add task 5 with route stats

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,6 +2,7 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from os import getenv
 
 
 app = Flask(__name__)
@@ -17,4 +18,10 @@ def close_session(self):
     storage.close()
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='5000', threaded=True)
+    host = getenv('HBNB_API_HOST')
+    port = getenv('HBNB_API_PORT')
+    if host is None:
+        host = '0.0.0.0'
+    if port is None:
+        port = '5000'
+    app.run(host=host, port=port)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,7 @@
 """ Module index """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status')
@@ -8,4 +9,17 @@ def status():
     """ status:
         return the status
     """
-    return jsonify(status = "OK")
+    return jsonify({'status': "OK"})
+
+
+@app_views.route('/stats')
+def stats():
+    """ stats:
+        return the number of each object
+    """
+    result = {}
+    objects = ['Amenity', 'City', 'Place', 'Review', 'State', 'User']
+    for i in objects:
+        result[i] = storage.count(i)
+
+    return jsonify(result)
